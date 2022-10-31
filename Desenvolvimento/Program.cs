5a2d7ba21c25977900c733325a26a3cd77b3c367
William Rodrigues <wrsilva008@gmail.com>
2022-10-31 11:05:39 -0300

    Primeiro commit da formação .Net

---
+++ Desenvolvimento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desenvolvimento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaDeIntroducao = "Introdução à coleção";
+            string aulaModelando = "Modelando tipos";
+            string aulaConfiguração = "Trabalhando com 'Sets'";
+
+            string[] aulas = new string[] {
+                aulaDeIntroducao,
+                aulaModelando,
+                aulaConfiguração
+            };
+
+            string[] aulas2 = new string[3];
+            aulas2[0] = aulaDeIntroducao;
+            aulas2[1] = aulaModelando;
+            aulas2[2] = aulaConfiguração;
+
+            foreach (var aula in aulas2)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
