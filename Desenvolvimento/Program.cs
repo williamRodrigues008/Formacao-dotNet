d5cce11b972dc79ece3da49ee9fee51a6021ffb7
William Rodrigues <wrsilva008@gmail.com>
2022-11-07 16:49:46 -0300

    Alteração do método para busca do primeiro e ultimo item com uma palavra filtro

--- Desenvolvimento/Program.cs
+++ Desenvolvimento/Program.cs
@@ -43,11 +43,21 @@ namespace Desenvolvimento
             listas.Add(aulaDeIntroducao);
             listas.Add(aulaModelando);
             listas.Add(aulaConfiguração);
-            //ImprimirLista(listas);
+            
+            ImprimirLista(listas);
 
+        }
+
+        private static void ImprimirLista(List<string> listas)
+        {
+
+            listas[0] = "Tipos de dados";
 
             Console.WriteLine("Primeira aula: " + listas.First());
+            Console.WriteLine("Primeira aula contendo tipos: " + listas.FirstOrDefault(aula => aula.Contains("Tipos")));
+
             Console.WriteLine("Ultima aula: " + listas.Last());
+            Console.WriteLine("Aulas contendo 'tipo', são ela(s): " + listas.LastOrDefault(aulas => aulas.Contains("tipos")));
 
             Console.WriteLine();
             Console.WriteLine();
@@ -57,14 +67,6 @@ namespace Desenvolvimento
                 Console.WriteLine(aula);
             });
             Console.WriteLine();
-        }
-
-        private static void ImprimirLista(List<string> listas)
-        {
-            foreach (var itensDaLista in listas)
-            {
-                Console.WriteLine(itensDaLista);
-            }
         }
     }
 }
